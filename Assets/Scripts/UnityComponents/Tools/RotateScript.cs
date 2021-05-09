974f715141e083c988a5ef140bb3e1a3f060d1a4
Evileptic <evileptic@gmail.com>
2021-05-10 00:23:22 +1000

    Fix crystal prefabs, fix blue crystal hole, rotate script

---
+++ Assets/Scripts/UnityComponents/Tools/RotateScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace MatchThree
+{
+    public class RotateScript : MonoBehaviour
+    {
+        public bool X, Y, Z;
+        public bool ReverseX, ReverseY, ReverseZ;
+        public Space RotateSpace;
+        public float Speed;
+
+        void Update()
+        {
+            float x = X ? (ReverseX ? Speed : -Speed) * Time.deltaTime : 0;
+            float y = Y ? (ReverseY ? Speed : -Speed) * Time.deltaTime : 0;
+            float z = Z ? (ReverseZ ? Speed : -Speed) * Time.deltaTime : 0;
+            transform.Rotate(x, y, z, RotateSpace);
+        }
+    }
+}
