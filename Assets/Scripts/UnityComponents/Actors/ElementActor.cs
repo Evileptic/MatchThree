984fb6573535a909596f978bb53fd91480739857
Evileptic <evileptic@gmail.com>
2021-05-11 23:52:59 +0900

    Elements blinking

--- Assets/Scripts/UnityComponents/Actors/ElementActor.cs
+++ Assets/Scripts/UnityComponents/Actors/ElementActor.cs
@@ -1,12 +1,24 @@
 using Leopotam.Ecs;
+using UnityEngine;
 
 namespace MatchThree
 {
+    [RequireComponent(typeof(Animator))]
     public class ElementActor : Actor
     {
+        public Animator Animator;
+
+        private void Start()
+        {
+            Animator = GetComponent<Animator>();
+        }
+
         public override void ExpandEntity(EcsEntity entity)
         {
             entity.Get<Element>().ActorRef = this;
         }
+
+        private void OnMouseEnter() => Animator.SetBool("Blink", true);
+        private void OnMouseExit() => Animator.SetBool("Blink", false);
     }
 }
